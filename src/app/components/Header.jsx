f8205cee739d4c51326087c342a3d23d5fb6e774
Veena <veenasahu195@gmail.com>
2024-08-27 21:17:00 +0530

    nav

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";  
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div class="logo">
+        <img src="/0e36691d40232362e5e738d445944622.png" alt="logo" />
+        MakeAdream
+      </div>
+        <div class="nav-links">
+          <div class="navopt">Home</div>
+          <div class="navopt">About Us</div>
+          <div class="navopt">Pricing</div>
+          <div class="navopt">Contact</div>
+          <div class="navopt">Blog</div>
+        </div>
+
+        <div class="au">
+        <div class="person-container">
+        <img src="/Person.png"></img>
+            <div class="popup-box">
+            <a href="#login">Login</a>
+            <a href="#signup">Signup</a>
+        </div>
+        </div>
+            <img src="/Search.png"></img>
+        </div>
+        
+
+    </header>
+    
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,30 @@
+// src/app/page.js
+import Header from "./components/Header";
+
+export default function HomePage() {
+  return (
+    <div className="container">
+      <Header/>
+      <section className="hero">
+        <h1>It’s A Big World Out There, Go Explore.</h1>
+        <p>Discover new attractions and experiences to match your interests and travel style.</p>
+        <button className="cta-button">Book Now</button>
+      </section>
+      <div class="card-container">
+    <div class="card">
+        <h3>Top Choices</h3>
+        <p>Total 600+ destinations you can work with.</p>
+    </div>
+    <div class="card">
+        <h3>Quality Guidance</h3>
+        <p>Our tour guide has 20+ years of experience.</p>
+    </div>
+    <div class="card">
+        <h3>Easy Bookings</h3>
+        <p>Best in class ticket booking system.</p>
+    </div>
+</div>
+    </div>
+    
+  );
+}
