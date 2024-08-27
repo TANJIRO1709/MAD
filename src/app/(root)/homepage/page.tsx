01cc2cd340f98d3fd1c37a4cd2051d40dcb26ebb
Veena <veenasahu195@gmail.com>
2024-08-27 21:48:12 +0530

    Commit the hero section of landing page.

--- src/app/(root)/homepage/page.tsx
+++ src/app/(root)/homepage/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.js
-import Header from "./components/Header";
+import Header from "@/app/components/Header";
 
 export default function HomePage() {
   return (
@@ -10,16 +10,16 @@ export default function HomePage() {
         <p>Discover new attractions and experiences to match your interests and travel style.</p>
         <button className="cta-button">Book Now</button>
       </section>
-      <div class="card-container">
-    <div class="card">
+      <div className="card-container">
+    <div className="card">
         <h3>Top Choices</h3>
         <p>Total 600+ destinations you can work with.</p>
     </div>
-    <div class="card">
+    <div className="card">
         <h3>Quality Guidance</h3>
         <p>Our tour guide has 20+ years of experience.</p>
     </div>
-    <div class="card">
+    <div className="card">
         <h3>Easy Bookings</h3>
         <p>Best in class ticket booking system.</p>
     </div>
