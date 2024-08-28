d1692fed9b4cbb1fb70e203dc9706bd8873e8c9b
Veena <veenasahu195@gmail.com>
2024-08-28 21:43:17 +0530

    Added the landing page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,33 +1,27 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Header() {
   return (
     <header className="header">
-      <div class="logo">
-        <img src="/0e36691d40232362e5e738d445944622.png" alt="logo" />
-        MakeAdream
+      <div className="logo">
+        <Image src="/0e36691d40232362e5e738d445944622.png" 
+        alt="logo" 
+        width={35} 
+        height={35}
+        className="md:mr-2 md:w-[50px] md:h-[50px] md:ml-2"
+        />
+        <p className="">Make A Dream</p>
       </div>
-        <div class="nav-links">
-          <div class="navopt">Home</div>
-          <div class="navopt">About Us</div>
-          <div class="navopt">Pricing</div>
-          <div class="navopt">Contact</div>
-          <div class="navopt">Blog</div>
-        </div>
-
-        <div class="au">
-        <div class="person-container">
+        <div className="au">
+        <div className="person-container">
         <img src="/Person.png"></img>
-            <div class="popup-box">
+            <div className="popup-box">
             <a href="#login">Login</a>
             <a href="#signup">Signup</a>
         </div>
         </div>
-            <img src="/Search.png"></img>
         </div>
-        
-
     </header>
-    
   );
 }
