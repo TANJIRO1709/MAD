29679a46d462a1e835ad37d5e2210902f2fdd0a9
somyaparidaa <somyaparida21@gmail.com>
2024-08-31 16:00:44 +0530

    something

--- src/app/components/ui/bento-grid.tsx
+++ src/app/components/ui/bento-grid.tsx
@@ -1,24 +1,6 @@
-import { cn } from "@/lib/utils";
 import { Sriracha } from "next/font/google";
 
-export const BentoGrid = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children?: React.ReactNode;
-}) => {
-  return (
-    <div
-      className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+const sriracha = Sriracha({ subsets: ["latin"], weight: "400" });
 
 export const BentoGridItem = ({
   className,
@@ -43,7 +25,7 @@ export const BentoGridItem = ({
       {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className={`${sriracha.className} font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2`}>
           {title}
         </div>
         <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
