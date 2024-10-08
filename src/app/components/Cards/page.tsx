4cc167a23bfb7f823df2fea5ae1832742e6f7fff
Tanjiro_1709 <147371895+TANJIRO1709@users.noreply.github.com>
2024-10-08 13:32:14 +0530

    Update page.tsx

--- src/app/components/Cards/page.tsx
+++ src/app/components/Cards/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // Add this if you are using hooks or any client-side features
 import React from "react";
-import { CanvasRevealEffect } from "@/app/components/ui/canvas-reveal-effect";
+import { CanvasRevealEffectDemo } from "@/app/components/ui/canvas-reveal-effect";
 import AceternityIcon from "@/app/components/icons"; // Adjust this import based on your file structure
 import { AnimatePresence, motion } from "framer-motion";
 import Icon from "@/app/components/icon"
