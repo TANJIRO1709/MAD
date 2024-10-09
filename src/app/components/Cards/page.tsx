6687f6dcbc4902f2a63c448f9c342d2290ebfc70
.Vibhuu <144808765+VVUx21@users.noreply.github.com>
2024-10-09 13:36:20 +0530

    Updated icon of page.tsx

--- src/app/components/Cards/page.tsx
+++ src/app/components/Cards/page.tsx
@@ -81,7 +81,7 @@ const AceternityIcon = () => {
   );
 };
  
-export const Icon = ({ className, ...rest }: any) => {
+const Icon = ({ className, ...rest }: any) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
